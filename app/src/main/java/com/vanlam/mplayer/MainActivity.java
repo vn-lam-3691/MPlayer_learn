8637873175c863d439a47afa352291252bd365f5
VanLamTruong <lamtruong3242@gmail.com>
2023-09-09 22:52:19 +0700

    get all file mp3 & display on ListView

--- app/src/main/java/com/vanlam/mplayer/MainActivity.java
+++ app/src/main/java/com/vanlam/mplayer/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -12,12 +18,19 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
 
         runtimePermission();
     }
@@ -27,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-
+                        displaySong();
                     }
 
                     @Override
@@ -37,8 +50,60 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-
+                        permissionToken.continuePermissionRequest();
                     }
                 }).check();
+    }
+
+    public ArrayList<File> findSong() {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File file = new File("/storage/emulated/0/Download");
+        File[] files = file.listFiles();
+
+        for (File singleFile : files) {
+            if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                arrayList.add(singleFile);
+            }
+        }
+
+        return arrayList;
+    }
+
+    public void displaySong() {
+        final ArrayList<File> mySongs = findSong();
+        items = new String[mySongs.size()];
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            View view = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView txtSong = view.findViewById(R.id.txtSong);
+            txtSong.setSelected(true);
+            txtSong.setText(items[position]);
+            return view;
+        }
     }
 }
